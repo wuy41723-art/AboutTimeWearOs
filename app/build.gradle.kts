e1f2e125e7155e26b2e32d42e82f6f788fac093d
wuy41723-art <wuy41723@gmail.com>
2026-07-30 10:00:51 +0800

    Create build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,69 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.abouttime.wearos"
+
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.abouttime.wearos"
+
+        minSdk = 30
+
+        targetSdk = 35
+
+        versionCode = 1
+
+        versionName = "1.0"
+    }
+
+    buildTypes {
+
+        release {
+
+            isMinifyEnabled = false
+
+            proguardFiles(
+                getDefaultProguardFile(
+                    "proguard-android-optimize.txt"
+                ),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    compileOptions {
+
+        sourceCompatibility = JavaVersion.VERSION_17
+
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+
+        jvmTarget = "17"
+    }
+}
+
+
+dependencies {
+
+    implementation(
+        "androidx.core:core-ktx:1.15.0"
+    )
+
+    implementation(
+        "androidx.wear:wear:1.3.0"
+    )
+
+    implementation(
+        "androidx.wear.watchface:watchface:1.2.1"
+    )
+
+    implementation(
+        "androidx.wear.watchface:watchface-client:1.2.1"
+    )
+}
